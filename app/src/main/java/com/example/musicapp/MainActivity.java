f5ecd2c64c1de2455499ca1a39ac22b59d0d0907
IsharaSandeepa <it20144258@my.sliit.lk>
2021-08-20 14:35:03 +0530

    MusicApp_V1

---
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView btnStart = (ImageView) findViewById(R.id.btnStart);
+        ImageView btnStop = (ImageView) findViewById(R.id.btnStop);
+        
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+}
